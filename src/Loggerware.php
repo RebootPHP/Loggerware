020713f7fd9537feda4ac90b57fb6c997a42ee21
Jan Kristian Valgijainen <54046100+ogchibi95@users.noreply.github.com>
2024-11-01 05:56:35 +0200

    Update Loggerware.php

--- src/Loggerware.php
+++ src/Loggerware.php
@@ -1,1 +1,28 @@
+<?php namespace RebootPHP;
 
+
+
+
+
+
+class Loggerware {
+
+
+
+		function create_log_file($fname) {
+
+      if(!file_exists($fname)) {
+        file_put_contents($fname, '');
+      } 
+
+      $ip = $_SERVER['REMOTE_ADDR'];
+      $time = date('d/m/y h:iA', time());
+
+      $log = file_get_contents($fname);
+      $line = '$ip\t$time\t$log\r';
+
+      file_put_contents($fname, $line);
+    } 
+
+
+} 
